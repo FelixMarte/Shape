d2c926c8039fa7fe46818bc1758ead806c6fced2
panbi <panbi@SHAIZA>
2024-09-10 19:45:58 -0400

    Agregar archivos de proyecto.

---
+++ Shape/Students.cs
@@ -0,0 +1,114 @@
+﻿
+using System.Collections.Generic;
+
+public abstract class Persona
+{
+    public string Nombre { get; set; }
+
+    public Persona(string nombre)
+    {
+        Nombre = nombre;
+    }
+
+    public string GetNombre()
+    {
+        return Nombre;
+    }
+}
+
+
+public class Estudiante : Persona
+{
+    public int NumeroUnico { get; set; }
+
+    public Estudiante(string nombre, int numeroUnico) : base(nombre)
+    {
+        NumeroUnico = numeroUnico;
+    }
+
+    public int GetNumeroUnico()
+    {
+        return NumeroUnico;
+    }
+}
+
+
+public class Profesor : Persona
+{
+    public List<Curso> Cursos { get; set; } = new List<Curso>();
+
+    public Profesor(string nombre) : base(nombre) { }
+
+    public void AsignarCurso(Curso curso)
+    {
+        Cursos.Add(curso);
+    }
+
+    public List<Curso> ObtenerCursos()
+    {
+        return Cursos;
+    }
+}
+
+
+public class Clase
+{
+    public string Identificador { get; set; }
+    public List<Estudiante> Estudiantes { get; set; } = new List<Estudiante>();
+    public List<Profesor> Profesores { get; set; } = new List<Profesor>();
+
+    public Clase(string identificador)
+    {
+        Identificador = identificador;
+    }
+
+    public void AgregarEstudiante(Estudiante estudiante)
+    {
+        Estudiantes.Add(estudiante);
+    }
+
+    public void AgregarProfesor(Profesor profesor)
+    {
+        Profesores.Add(profesor);
+    }
+
+    public List<Estudiante> ObtenerEstudiantes()
+    {
+        return Estudiantes;
+    }
+
+    public List<Profesor> ObtenerProfesores()
+    {
+        return Profesores;
+    }
+}
+
+
+public class Curso
+{
+    public string Nombre { get; set; }
+    public int RecuentoClases { get; set; }
+    public int RecuentoEjercicios { get; set; }
+
+    public Curso(string nombre, int recuentoClases, int recuentoEjercicios)
+    {
+        Nombre = nombre;
+        RecuentoClases = recuentoClases;
+        RecuentoEjercicios = recuentoEjercicios;
+    }
+
+    public string ObtenerNombre()
+    {
+        return Nombre;
+    }
+
+    public int ObtenerRecuentoClases()
+    {
+        return RecuentoClases;
+    }
+
+    public int ObtenerRecuentoEjercicios()
+    {
+        return RecuentoEjercicios;
+    }
+}
---
+++ Shape/shapes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public abstract class Shapes
+{
+    public double Ancho { get; set; }
+    public double Alto { get; set; }
+
+    public Shapes(double ancho, double alto)
+    {
+        Ancho = ancho;
+        Alto = alto;
+    }
+
+    public abstract double CalculateSurface();
+}
+
+public class Rectangle : Shapes
+{
+    public Rectangle(double ancho, double alto) : base(ancho, alto) { }
+
+    public override double CalculateSurface()
+    {
+        return Ancho * Alto;
+    }
+}
+
+public class Triangle : Shapes
+{
+    public Triangle(double ancho, double alto) : base(ancho, alto) { }
+
+    public override double CalculateSurface()
+    {
+        return (Ancho * Alto) / 2;
+    }
+}
+
+public class Circle : Shapes
+{
+    public Circle(double radio) : base(radio, radio) { }
+
+    public override double CalculateSurface()
+    {
+        return Math.PI * Math.Pow(Ancho, 2);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ejemplo de figuras geométricas
+        Shapes[] shapes = new Shapes[]
+        {
+            new Rectangle(4, 5),
+            new Triangle(4, 5),
+            new Circle(3)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Área de {shape.GetType().Name}: {shape.CalculateSurface()}");
+        }
+    }
+}
